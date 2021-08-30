2fac1d0959df5f99dbe8af9203d99a7933904b46
RinseV <rinse@vlaswinkel.info>
2021-08-30 16:27:49 +0200

    Updated ping command to show roundtrip latency

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,9 +1,10 @@
-import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, Message } from 'discord.js';
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with roundtrip latency'),
     async execute(interaction: CommandInteraction) {
-        return interaction.reply('Pong!');
+        const sent = (await interaction.reply({ content: 'Pinging...', fetchReply: true })) as Message;
+        interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
     }
 };
